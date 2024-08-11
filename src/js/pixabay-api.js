0e1f60a52d79fe8a5078656ca0ce513e2d23d07c
Artur Prepelytsia <preart81@gmail.com>
2024-08-11 17:47:00 +0300

    final
    - add params to AXIOS
    - add func showGallery in main.js
    - add scroll to new images after Load More
    - add loader after Load More

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -2,19 +2,18 @@ import axios from 'axios';
 
 const API_KEY = '45270476-0e4dc869b670f41fd1cb8efc7';
 
-export const fetchImages = async (queryText, page = 1) => {
-  const searchParams = new URLSearchParams({
-    key: API_KEY,
-    q: queryText,
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: true,
-    // safesearch: false,
-    page: page,
-    per_page: 15,
+export const fetchImages = async (queryText, page = 1, per_page = 15) => {
+  const response = await axios.get('https://pixabay.com/api/?', {
+    params: {
+      key: API_KEY,
+      q: queryText,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+      // safesearch: false,
+      page: page,
+      per_page: per_page,
+    },
   });
-
-  const response = await axios.get(`https://pixabay.com/api/?${searchParams}`);
-  console.log(response);
   return response.data;
 };
--- src/main.js
+++ src/main.js
@@ -5,17 +5,75 @@ import { markup } from './js/render-functions';
 
 const MSG_NO_RESULTS =
   'Sorry, there are no images matching your search query. Please try again.';
+const MSG_END_OF_SEARCH =
+  'We are sorry, but you have reached the end of search results.';
 
 const searchForm = document.querySelector('.search-form');
 const searchInput = document.querySelector('.search-text');
 const gallery = document.querySelector('.gallery');
 const loader = document.querySelector('.loader');
+const btnLoadMore = document.querySelector('.btn-load-more');
 
+let page = 1;
+const img_per_page = 15;
 let searhText = '';
+
+const showGallery = searhText => {
+  btnLoadMore.classList.add('is-hidden');
+  loader.style.display = 'block';
+
+  pixabayFetchImages(searhText, page, img_per_page)
+    .then(images => {
+      if (images.hits.length === 0) {
+        iziToast.error({
+          position: 'topRight',
+          title: 'Error',
+          message: MSG_NO_RESULTS,
+        });
+        return;
+      }
+      const rect2 = gallery.getBoundingClientRect();
+      gallery.insertAdjacentHTML('beforeend', markup(images.hits));
+      galleryLightbox.refresh();
+
+      if (
+        images.hits.length < img_per_page ||
+        img_per_page * page >= images.totalHits
+      ) {
+        iziToast.error({
+          position: 'topRight',
+          title: 'Error',
+          message: MSG_END_OF_SEARCH,
+        });
+      } else {
+        btnLoadMore.classList.remove('is-hidden');
+      }
+      if (page > 1) {
+        const galleryCard = document.querySelector('.gallery-card');
+        const rect = galleryCard.getBoundingClientRect();
+
+        console.log(rect2);
+
+        const scrollHeight = rect.height * 2;
+        // // прокуртка на 2 висоти зображення як в завданні
+        // window.scrollBy({ top: scrollHeight, behavior: 'smooth' });
+        // пркурутка до нових зображень
+        window.scrollBy({ top: rect2.bottom, behavior: 'smooth' });
+      }
+
+      // console.log(rect);
+    })
+    .catch(error => {
+      console.log(error);
+    })
+    .finally(() => {
+      loader.style.display = 'none';
+    });
+};
 
 const onSearch = event => {
   event.preventDefault();
-  const searhText = searchInput.value.trim();
+  searhText = searchInput.value.trim();
   searchInput.value = '';
 
   if (searhText === '') {
@@ -28,34 +86,17 @@ const onSearch = event => {
   }
 
   gallery.innerHTML = '';
-  loader.style.display = 'block';
+  page = 1;
+  showGallery(searhText);
+};
 
-  let page = 1;
-
-  pixabayFetchImages(searhText)
-    .then(images => {
-      if (images.hits.length === 0) {
-        iziToast.error({
-          position: 'topRight',
-          title: 'Error',
-          message: MSG_NO_RESULTS,
-        });
-        return;
-      }
-
-      gallery.innerHTML = markup(images.hits);
-
-      galleryLightbox.refresh();
-    })
-    .catch(error => {
-      console.log(error);
-    })
-    .finally(() => {
-      loader.style.display = 'none';
-    });
+const onLoadMore = () => {
+  page += 1;
+  showGallery(searhText);
 };
 
 searchForm.addEventListener('submit', onSearch);
+btnLoadMore.addEventListener('click', onLoadMore);
 
 let galleryLightbox = new SimpleLightbox('.gallery a', {
   captions: true,
